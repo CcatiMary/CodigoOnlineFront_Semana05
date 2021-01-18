da2d6353ec00444bd765f3936a94de950524066c
CcatiMary <myccasanit@gmail.com>
2021-01-18 00:02:16 -0500

    cambio1

---
+++ reto1.js
@@ -0,0 +1,13 @@
+var litros= +prompt("Ingrese los litros")
+var precio=+prompt("Ingrese precio por galon")
+
+
+function convertir_precio(){
+   var t = lit/3.785
+   var g = g*PreGal
+   return g
+
+}
+
+var g= convertir()
+console.log("la ganancia en día es " +g)
